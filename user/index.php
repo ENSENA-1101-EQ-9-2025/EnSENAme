2bcfd4c56b43f7c81d17b8471846c1d754321715
Jeremy Chica Tapasco <jeremy.chicatap@iefedericoozanam.edu.co>
2025-10-01 11:48:57 -0500

    cambio al chat

--- user/index.php
+++ user/index.php
@@ -449,7 +449,7 @@ En el entorno laboral, contribuiría a una mayor integración de personas sordas
   <!-- Required Js -->
   <script src="../assets/js/plugins/popper.min.js"></script>
   <script src="../assets/js/plugins/simplebar.min.js"></script>
-  <script src="../admin/assets/js/plugins/bootstrap.min.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
   <script src="../assets/js/fonts/custom-font.js"></script>
   <script src="../assets/js/pcoded.js"></script>
   <script src="../assets/js/plugins/feather.min.js"></script>
